d20ff280f1a6a3b71bae1803a82529532bb4eb53
Nils Alberding-Schulz <nilsalberding@web.de>
2025-07-18 15:04:11 +0200

    add Class Pokemon and function getPokemonFromApi

---
+++ script.js
@@ -0,0 +1,148 @@
+// async function renderCards() {
+
+//     const cardSectionRef = document.getElementById('cards');
+
+//     for (let i = 1; i < 10; i++) {
+
+//         const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + i);
+//         const pokeJson = await pokeResponse.json();
+
+//         console.log(pokeJson);
+
+
+
+//         cardSectionRef.innerHTML += /*html*/`                
+//                 <div class="card">
+//                     <img src="${pokeJson.sprites.front_default}" alt="">
+//                     <div class="card-details">
+//                         <span>#${i}</span>
+//                         <span>${pokeJson.name}</span>
+//                         <div id='types${i}'>
+//                             <span>Fire</span>
+//                             <span>Flying</span>
+//                         </div>
+
+//                     </div>
+//                 </div>
+//         `
+
+//     }
+
+// }
+
+// renderCards();
+
+// // TODO: next step -- types rendern
+
+// async function renderTypes() {
+
+//     for (let j = 0; j < pokeJson.types.length; j++) {
+
+//         const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + j);
+//         const pokeJson = await pokeResponse.json();
+
+//         const typeRef = document.getElementById('type' + j);
+
+//         typeRef.innerHTML +=  /*html*/`
+//                 <span>${pokeJson.types[j].type.name}</span>
+//             `
+
+//     }
+// }
+// renderTypes()
+// const pokeArray = [];;
+
+
+
+class Pokemon {
+
+    // #region attributes
+
+    name;
+    spriteSrc;
+    id;
+    types = [];
+    stats = {
+        "hp": 0,
+        "attack": 0,
+        "defense": 0,
+        "special_attack": 0,
+        "special_defense": 0,
+        "speed": 0
+    };
+    abilities = [];
+    weight;
+    height;
+
+// #endregion
+
+    // constructor erstellen, in dem die methoden aufgerufen werden und die einfachen attributes erzeugt werden
+    // benötigte parameter des constructors? 
+    constructor({ pName, pSpriteSrc, pIndex, pTypes, pStats, pAbilities, pWeight, pHeight }) {
+
+        this.spriteSrc = pSpriteSrc;
+        this.id = pIndex;
+        this.weight = pWeight;
+        this.height = pHeight;
+
+        this.getAbilities(pAbilities);
+        this.getStats(pStats);
+        this.getTypes(pTypes);
+        this.getUpperName(pName)
+    }
+
+    // methoden erstellen, um an werte für stats sowie types zu kommen
+    // Welche Parameter brauchen die methoden?
+
+    // #region methods
+
+    getStats(pokeStats) {
+
+        this.stats.hp = pokeStats[0].base_stat;
+        this.stats.attack = pokeStats[1].base_stat;
+        this.stats.defense = pokeStats[2].base_stat;
+        this.stats.special_attack = pokeStats[3].base_stat;
+        this.stats.special_defense = pokeStats[4].base_stat;
+        this.stats.speed = pokeStats[5].base_stat;
+    }
+
+    getAbilities(pokeAbilities) {
+        for (let i = 0; i < pokeAbilities.length; i++) {
+            this.abilities.push(pokeAbilities[i].ability.name);
+        }
+    }
+
+    getTypes(pokeTypes) {
+        for (let i = 0; i < pokeTypes.length; i++) {
+            this.types.push(pokeTypes[i].type.name);
+        }
+    }
+
+    getUpperName(pName){
+        const word = pName;
+        this.name = word.charAt(0).toUpperCase() + word.slice(1);        
+    }
+
+    
+    // #endregion
+}
+
+const pokeArray = [];
+
+async function getPokemonFromApi() {
+
+    for (let i = 1; i <= 10; i++) {
+
+        const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + i);
+        const pokeJson = await pokeResponse.json();
+
+        pokeArray.push(new Pokemon({pAbilities: pokeJson.abilities, pName: pokeJson.name, pHeight: pokeJson.height, pIndex: i, pSpriteSrc: pokeJson.sprites.front_default, pStats: pokeJson.stats, pTypes: pokeJson.types, pWeight: pokeJson.weight}));
+    }
+
+    // console.log(new Pokemon({pAbilities: pokeArrayAll[0].abilities, pName: pokeArrayAll[0].name, pHeight: pokeArrayAll[0].height, pIndex: 0, pSpriteSrc: pokeArrayAll[0].sprites.front_default, pStats: pokeArrayAll[0].stats, pTypes: pokeArrayAll[0].types, pWeight: pokeArrayAll[0].weight}));
+
+}
+
+getPokemonFromApi();
+
+console.log(pokeArray);
